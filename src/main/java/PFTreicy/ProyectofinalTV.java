29fe3b3a85323eb5c365508db1b79f7fde2c4f7e
daine <daine@192.168.1.49>
2025-09-30 21:58:15 -0500

    carga inicial

---
+++ src/main/java/PFTreicy/ProyectofinalTV.java
@@ -0,0 +1,30 @@
+package PFTreicy;
+
+import java.util.Scanner;
+public class ProyectofinalTV{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese el nombre del cliente: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Ingrese el monto de la primera compra: ");
+        double compra1 = sc.nextDouble();
+
+        System.out.print("Ingrese el monto de la segunda compra: ");
+        double compra2 = sc.nextDouble();
+
+        System.out.print("Ingrese el monto de la tercera compra: ");
+        double compra3 = sc.nextDouble();
+
+        double totalCompras = compra1 + compra2 + compra3;
+        double promedioCompras = totalCompras / 3;
+
+        System.out.println("\n--- Resultados ---");
+        System.out.println("Cliente: " + nombre);
+        System.out.println("Total de compras     : " + totalCompras);
+        System.out.println("Promedio de compras  : " + promedioCompras);
+
+        sc.close();
+    }
+}
